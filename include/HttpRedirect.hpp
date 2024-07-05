59ea6c796e6a1986029a22cc3acbdb4bd5c8a628
Alexis LOUBIERE <alex.loubiere@gmail.com>
2024-07-05 14:27:38 +0200

    added HttpRedirect class to handle redirections

--- include/HttpRedirect.hpp
+++ include/HttpRedirect.hpp
@@ -1,17 +1,21 @@
 #pragma once
 
 #include <string>
-// Used as an interface between the request and the response allowing to define the location
+class Response;
+class Request;
+// Used as an interface between the request and the response allowing to define
+// the location
 class HttpRedirect {
 public:
-    HttpRedirect();
-    ~HttpRedirect();
-    HttpRedirect(HttpRedirect const &src);
-    HttpRedirect& operator=(HttpRedirect const &rhs);
-    void redirect();
-    bool isRedirect();
-    std::string const &location();
+  HttpRedirect();
+  ~HttpRedirect();
+  HttpRedirect(HttpRedirect const &src);
+  HttpRedirect &operator=(HttpRedirect const &rhs);
+  static void handleRedirect(Request const &req, Response &response);
+  bool isRedirect();
+  std::string const &location();
+
 private:
-    std::string _location;
-    bool        _redirect;
+  std::string _location;
+  bool _redirect;
 };
