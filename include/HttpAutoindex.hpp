141ef238f077bf048cb1e378e11edddc854afae7
Alexis LOUBIERE <alex.loubiere@gmail.com>
2024-07-06 16:15:54 +0200

    adding HttpAutoindex class

---
+++ include/HttpAutoindex.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+class HttpAutoindex {
+public:
+    HttpAutoindex();
+    ~HttpAutoindex();
+    HttpAutoindex(HttpAutoindex const &src);
+    HttpAutoindex& operator=(HttpAutoindex const &rhs);
+private:
+};
---
+++ src/HttpAutoindex.cpp
@@ -0,0 +1,17 @@
+#include "HttpAutoindex.hpp"
+HttpAutoindex::HttpAutoindex(){
+}
+
+HttpAutoindex::~HttpAutoindex(){
+}
+
+HttpAutoindex::HttpAutoindex(HttpAutoindex const &src){
+    (void) src;
+}
+
+HttpAutoindex& HttpAutoindex::operator=(HttpAutoindex const &rhs){
+    (void)rhs;
+    return *this;
+}
+
+
