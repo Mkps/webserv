5dc4f982d4d3d97eba5a9ef16c4294cc07baca89
Mkps <alex.loubiere@gmail.com>
2024-07-09 02:04:33 +0200

    adding autoindex class

--- src/HttpAutoindex.cpp
+++ src/HttpAutoindex.cpp
@@ -1,4 +1,11 @@
 #include "HttpAutoindex.hpp"
+#include "Response.hpp"
+#include <sstream>
+#include <dirent.h>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
 HttpAutoindex::HttpAutoindex(){
 }
 
@@ -14,4 +21,31 @@ HttpAutoindex& HttpAutoindex::operator=(HttpAutoindex const &rhs){
     return *this;
 }
 
-
+inline std::string listDirectories(std::string const &req_path, std::string const &path) {
+    std::vector<std::string> v;
+    DIR* dirp = opendir(path.c_str());
+    struct dirent * dp;
+    while ((dp = readdir(dirp)) != NULL) {
+        std::cout << dp->d_name << std::endl;
+        v.push_back(dp->d_name);
+    }
+    closedir(dirp);
+    std::ostringstream ss;
+    std::sort(v.begin(), v.end());
+    while (!v.empty()) {
+        ss << "<p><a href=\"http://localhost:8000/" << req_path << v.front() << "\">" << v.front() << "</a></p>\n";
+        v.erase(v.begin());
+    }
+    return ss.str();
+}
+std::string HttpAutoindex::generateIndex(std::string const &req_path, std::string const &path){
+    std::ostringstream ss; 
+    if (path.empty())
+        ss << "<DOCTYPE !html><html><body>Index not found." << "</body></html>";
+    else {
+        ss << "<DOCTYPE !html><html><body><h1>Index of " << path << "</h1>";
+        ss << listDirectories(req_path, path);
+        ss << "</body></html>";
+    }
+    return ss.str();
+}
