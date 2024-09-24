212ecb5505ce313c0aba0ab9f84daaf58e01b83d
Mkps <alex.loubiere@gmail.com>
2024-09-24 13:32:06 +0200

    added bad + infinite python scripts

---
+++ resources/html/infinite.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+from os import environ
+import cgi, cgitb 
+
+# Create instance of FieldStorage 
+form = cgi.FieldStorage() 
+
+# Get data from fields
+while True:
+    pass
